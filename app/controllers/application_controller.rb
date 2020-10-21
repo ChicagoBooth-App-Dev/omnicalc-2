1b72b1ee4f447f0a21f7a37d5a86a2b94a660be6
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2020-10-21 07:24:39 +0000

    almost done with add subtract multiply and divide tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,71 @@
 class ApplicationController < ActionController::Base
 
   def add_form
-    render({:template => 'calculations/add_template.html.erb'})
+    @operation = 'addition'
+    @op1 = 'Add'
+    @op2 = 'to'
+    @calculation = '/wizard_add'
+    render({:template => 'calculations/form_template.html.erb'})
   end
 
   def add_results
-    render({:template => 'calculations/add_results.html.erb'})
+    @operation = 'Addition'
+    @num1 = params.fetch('num1').to_f
+    @num2 = params.fetch('num2').to_f
+    @result = @num1 + @num2
+    @home = "/add"
+    render({:template => 'calculations/calculation_results.html.erb'})
   end
+
+  def subtract_form
+    @operation = 'subtraction'
+    @op1 = 'Subtract'
+    @op2 = 'from'
+    @calculation = '/wizard_subtract'
+    render({:template => 'calculations/form_template.html.erb'})
+  end
+
+  def subtract_results
+    @operation = 'Subtraction'
+    @num1 = params.fetch('num1').to_f
+    @num2 = params.fetch('num2').to_f
+    @result = @num1 - @num2
+    @home = "/subtract"
+    render({:template => 'calculations/calculation_results.html.erb'})
+  end
+
+  def multiply_form
+    @operation = 'multiplication'
+    @op1 = 'Multiply'
+    @op2 = 'by'
+    @calculation = '/wizard_multiply'
+    render({:template => 'calculations/form_template.html.erb'})
+  end
+
+  def multiply_results
+    @operation = 'Multiplication'
+    @num1 = params.fetch('num1').to_f
+    @num2 = params.fetch('num2').to_f
+    @result = @num1 * @num2
+    @home = "/multiply"
+    render({:template => 'calculations/calculation_results.html.erb'})
+  end
+
+  def divide_form
+    @operation = 'division'
+    @op1 = 'Divide'
+    @op2 = 'by'
+    @calculation = '/wizard_divide'
+    render({:template => 'calculations/form_template.html.erb'})
+  end
+
+  def divide_results
+    @operation = 'Division'
+    @num1 = params.fetch('num1').to_f
+    @num2 = params.fetch('num2').to_f
+    @result = @num1 / @num2
+    @home = "/divide"
+    render({:template => 'calculations/calculation_results.html.erb'})
+  end  
+
 end
