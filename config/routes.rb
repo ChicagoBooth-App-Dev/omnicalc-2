d55e646ac9a20a6d04c72e2a33dfa7c0298783f7
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2020-10-22 01:41:59 +0000

    street to forms RCAV done

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,22 @@ Rails.application.routes.draw do
 
   get('/wizard_divide', { :controller => 'application', :action => 'divide_results'})
 
+  #Def action pending
+  get("/muggle_translate", {:controller => 'api', :action=>'translate'})
+        
+  get("/street_to_coords/new", {:controller => 'api', :action => 'street_to_coords_form'})
+
+  get("/street_to_coords/results", {:controller => 'api', :action => 'street_to_coords_results'})
+        
+  get("/coords_to_weather/new", {:controller => 'api', :action => 'coords_to_weather_form'})
+
+  get("/coords_to_weather/results", {:controller => 'api', :action => 'coords_to_weather_results'})
+        
+  get("/street_to_weather/new", {:controller => 'api', :action => 'street_to_weather_form'})
+
+  get("/street_to_weather/results", {:controller => 'api', :action => 'street_to_weather_results'})
+        
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
